536f47fe8258d9ccf1694838b2d0f4dc5ddfa38a
Tsubasa Sakai <javascripter@tsukkun.net>
2023-09-25 22:54:44 +0900

    Fix crash when using WithSkiaWeb component

--- package/src/web/WithSkiaWeb.tsx
+++ package/src/web/WithSkiaWeb.tsx
@@ -1,5 +1,5 @@
 import type { ComponentProps, ComponentType } from "react";
-import React, { useMemo, lazy, Suspense } from "react";
+import React, { useState, useEffect, useMemo, lazy, Suspense } from "react";
 
 import { Platform } from "../Platform";
 
@@ -16,14 +16,13 @@ export const WithSkiaWeb = ({
   fallback,
   opts,
 }: WithSkiaProps) => {
+  const [isSkiaLoaded, setIsSkiaLoaded] = useState(false);
   const Inner = useMemo(
     // TODO: investigate
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (): any =>
       lazy(async () => {
-        if (Platform.OS === "web") {
-          await LoadSkiaWeb(opts);
-        } else {
+        if (Platform.OS !== "web") {
           console.warn(
             "<WithSkiaWeb /> is only necessary on web. Consider not using on native."
           );
@@ -32,6 +31,24 @@ export const WithSkiaWeb = ({
       }),
     [getComponent, opts]
   );
+
+  useEffect(() => {
+    LoadSkiaWeb(opts)
+      .then(() => {
+        setIsSkiaLoaded(true);
+      })
+      .catch((error) => {
+        // Throwing inside setState surfaces the error to the nearest error boundary
+        setIsSkiaLoaded(() => {
+          throw error;
+        });
+      });
+  }, [opts]);
+
+  if (!isSkiaLoaded) {
+    return <>{fallback}</>;
+  }
+
   return (
     <Suspense fallback={fallback ?? null}>
       <Inner />
